f8053f805235c44d08f1d92ffc9f15bb517e3293
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-31 14:26:36 -0700

    added gems and keys

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'minitest-vcr'
+  gem 'webmock'
+  gem 'dotenv-rails'
 end
 
 group :development do
@@ -79,3 +82,5 @@ group :test do
   gem 'minitest-rails'
   gem 'minitest-reporters'
 end
+
+gem 'httparty'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,31 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"require "minitest/reporters"  # for Colorized output
 #  For colorful output!
+require 'vcr'
+require 'webmock/minitest'
+
+
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
   ENV,
   Minitest.backtrace_filter
 )
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes' # folder where casettes will be located
+  config.hook_into :webmock # tie into this other tool called webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,    # record new data when we don't have it yet
+    :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
+  }
+  # Don't leave our Slack token lying around in a cassette file.
+  config.filter_sensitive_data("<APP_ID>") do
+    ENV['APP_ID']
+  end
+  config.filter_sensitive_data("<APP_KEY>") do
+    ENV['APP_KEY']
+  end
+end
 
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
